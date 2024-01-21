db153f3ce9e21d2b2a79d6b63678e62c5ff5a8e9
Victor-Weng <vweng@tfs.ca>
2024-01-20 19:52:43 -0500

    Changes to interface and rendering

--- app/screens/Overview.tsx
+++ app/screens/Overview.tsx
@@ -1,34 +1,96 @@
-import { View, Text, Button, StyleSheet, TextInput } from "react-native"; // generate with rnfe
+import { View, Text, Button, StyleSheet, TextInput, FlatList, TouchableOpacity } from "react-native"; // generate with rnfe
 import React, { useEffect, useState } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { FIRESTORE_DB } from "../../firebaseConfig";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
+export interface Time {
+  title: string;
+  done: boolean;
+  id: string;
+}
 const Overview = ({ navigation }: any) => {
-  const [times, setTimes] = useState<any[]>([]);
-  const [time, setTime] = useState('');
+  const [times, setTimes] = useState<Time[]>([]);
+  const [time, setTime] = useState("");
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const timeRef = collection(FIRESTORE_DB, 'times');
+
+    const subscriber = onSnapshot(timeRef, {
+      next: (snapshot) => {
+        console.log('UPDATED');
+
+        const times: Time[] = [];
+        snapshot.docs.forEach((doc) => {
+          times.push({
+            id: doc.id,
+            ...doc.data(),
+          } as Time);
+        });
+        setTimes(times);
+      },
+    });
+
+    return () => subscriber();
+
+  }, []);
 
   const addTime = async () => {
     console.log("ADD");
     try {
-      const doc = addDoc(collection(FIRESTORE_DB, "Times"), { // ERROR
-        title: "I am a test",
-        done: false
+      const doc = addDoc(collection(FIRESTORE_DB, 'times'), {
+        // ERROR
+        title: "time",
+        done: false,
       });
     } catch (err) {
       console.error("addDoc failed. reason :", err);
+      setTime("");
     }
   };
-  // declared below const styes ...
-  return (
-    <View style={styes.container}> 
-      <View style={styes.form}>
-        <Button onPress={() => addTime()} title="Add times" />
-        <TextInput style={styes.input} placeholder='Add new time' 
-        onChangeText={(text: string) => setTime(text)} value={time} />
-        <Button onPress={addTime} title="Add Time" disabled={time === ''} />
+
+  const renderTime = ({item } : any) => {
+    const toggleDone = async() => {
+
+    }
+
+    const deleteItem = async() => {
+      
+    }
+    return (
+      <View>
+        <TouchableOpacity onPress={toggleDone}>
+          {item.done && <Ionicons name='md-checkmark-circle' />}
+
+            </Ionicons>}
+          <Text>{item.title}</Text>
+        </TouchableOpacity>
       </View>
+    )
+  }
+
+  // declared below const styes ...
+  
+  return (
+    <View style={styes.container}>
+      <View style={styes.form}>
+        <TextInput
+          style={styes.input}
+          placeholder="Add new time"
+          onChangeText={(text: string) => setTime(text)}
+          value={time}
+        />
+        <Button onPress={addTime} title="Add Time" disabled={time === ""} />
+      </View>
+      { times.length > 0 && (
+        <View>
+          <FlatList
+          data={times}
+          renderItem={(item) => renderTime(item)}
+          keyExtractor={(time: Time) => time.id}
+          />
+      </View>
+      )}
     </View>
   );
 };
@@ -40,8 +102,8 @@ const styes = StyleSheet.create({
     marginHorizontal: 20,
   },
   form: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   input: {
     flex: 1,
@@ -49,7 +111,6 @@ const styes = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 4,
     padding: 10,
-    backgroundColor: '#fff', //#1F3B4D
-
-  }
-})
+    backgroundColor: "#fff", //#1F3B4D
+  },
+});
